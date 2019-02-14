73f4c654258ec84a570e22e6316142d64754d7b0
Joanna Summerlin <jksummerlin1@buffs.wtamu.edu>
2019-02-14 12:15:05 -0600

    Initialized commit

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new MovieContext())
+            {
+                // Useful tactic ONLY in development.
+                // At start of your program, always delete the database and then re-create it
+                // This ensures a fresh database everytime you run your program.
+                db.Database.EnsureDeleted();
+                db.Database.EnsureCreated();
+            }
+            
+            // Adding a studio: "20th Century Fox" with 4 movies
+            using (var db = new MovieContext())
+            {
+                Studio studio = new Studio
+                {
+                    Name = "20th Century Fox",
+                    Movies = new List<Movie>
+                    {
+                        new Movie {Title = "Avatar", Genre = "Action"},
+                        new Movie {Title = "Deadpool", Genre = "Action"},
+                        new Movie {Title = "Apollo 13", Genre = "Drama"},
+                        new Movie {Title = "The Martian", Genre = "Sci-Fi"}
+                    }
+                };
+
+                // Adding a studio: "Universal Pictures" with no movies
+                Studio studio2 = new Studio
+                {
+                    Name = "Universal Pictures"
+                };
+
+                db.Add(studio);
+                db.Add(studio2);
+                db.SaveChanges();
+            }
+
+            // Adding a movie: "Jurassic Park" to the Universal Pictures Studio
+            using (var db = new MovieContext())
+            {
+                Movie movie = new Movie
+                {
+                    Title = "Jurassic Park", Genre = "Action"
+                };
+                
+                Studio studioToUpdate = db.Studios.Include(s => s.Movies).Where(s => s.StudioId == 2).First();
+                studioToUpdate.M.Add(movie);
+                db.SaveChanges();
+            }
+        }
+    }
+}
---
+++ model.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab6
+{
+    public class MovieContext : DbContext // ":" means inheritance
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source = database.db");
+        }
+
+        public DbSet<Movie> Movies {get; set;}
+        public DbSet<Studio> Studios {get; set;}
+    }    
+        
+    public class Studio
+    {
+        public int StudioId {get; set;}
+        public string Name {get; set;}
+
+        public List<Movie> Movies {get; set;}
+
+        public override string ToString()
+        {
+            return $"Studio: {Name}";
+        }
+    }
+    public class Movie
+    {
+        public int MovieID {get; set;}
+        public string Title {get; set;}
+        public string Genre {get; set;}
+        public int StudioId {get; set;}
+
+        public override string ToString()
+        {
+            return $"Movie: {Title}";
+        }
+    }
+
+
+
+}
